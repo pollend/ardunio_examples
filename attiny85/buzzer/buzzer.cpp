0bdadd30817ef74123040b4be94cd69c447a8e6d
Michael Pollind <mpollind@gmail.com>
2019-01-14 10:29:44 -0800

    added updated code

--- attiny85/buzzer/buzzer.cpp
+++ attiny85/buzzer/buzzer.cpp
@@ -1,14 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
-#define HERTZ 220.0f
-#define OCTIVE  4
+#include <avr/interrupt.h>
 
 int main(void){
-    DDRB = (1 << PB3);
-    GTCCR = 0x90 | (8 - OCTIVE);
-    OCR1B = HERTZ - 1
-    while(1){
-    }
+	DDRB   |= (1 << PB0);                   // PWM output on PB2
+    TCCR0A = (1 << COM0A1) | (1 << WGM00);  // phase correct PWM mode
+
+    TCCR0B = (1 << CS01) | (1 << CS00);   // clock source = CLK/8, start PWM
+    OCR0A  = 126;                          // initial PWM pulse width
+	
+    while(1){}
+
+	
     return 1;
 }
