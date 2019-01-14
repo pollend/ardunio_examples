23d71c7b5d4750f2dc7a1ff5cd04f9b42478b5ba
Michael Pollind <polli104@mail.chapman.edu>
2019-01-14 11:02:19 -0800

    added readme

--- attiny85/buzzer/buzzer.cpp
+++ attiny85/buzzer/buzzer.cpp
@@ -6,8 +6,13 @@ int main(void){
 	DDRB   |= (1 << PB0);                   // PWM output on PB2
     TCCR0A = (1 << COM0A1) | (1 << WGM00);  // phase correct PWM mode
 
-    TCCR0B = (1 << CS01) | (1 << CS00);   // clock source = CLK/8, start PWM
-    OCR0A  = 126;                          // initial PWM pulse width
+    // TCCR0B =  (1 << CS00);                 // clock source = CLK/1, no prescaling
+    // TCCR0B =  (1 << CS01);                 // clock source = CLK/8, no prescaling
+    // TCCR0B =  (1 << CS01) | (1 << CS00);   // clock source = CLK/64, no prescaling
+    // TCCR0B =  (1 << CS02);                 // clock source = CLK/256, no prescaling
+    // TCCR0B =  (1 << CS00) | (1 << CS02);   // clock source = CLK/1024, no prescaling
+    
+    OCR0A  = 126;                             // initial PWM pulse width
 	
     while(1){}
 
